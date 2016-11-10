d3729588f8ef024adcdffd4339c329cdb8d1cd48
DoRan <imscsc21@naver.com>
2016-11-10 22:19:20 +0900

    Add files via upload

--- upload.sh
+++ upload.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cd ~/Desktop
+python3 wordchain.py
 cd $1
 git config user.name "hsh97Erica"
 git config user.email "imscs21@hanyang.ac.kr"
@@ -8,9 +10,11 @@ time=60
 #do
 git init
 git pull origin
-echo $(date) >> a.test
-echo "" >>a.test
-git add a.test
+cp ~/Desktop/chain.txt ./
+
+#echo $(date) >> a.test
+#echo "" >>a.test
+git add chain.txt
 git commit -m "upload $(date)"
 git push origin master
 #sleep $time
---
+++ wordchain.py
@@ -0,0 +1,56 @@
+def doWordChain():
+    import random
+    def getNewWord(output,data):
+        print("gen new game")
+        next_word=data[random.randint(0,len(dict)-1)]
+        output.write(next_word+"\n")
+        return next_word
+    
+    dictfp = open("dictionary.txt","r")
+    output = open("chain.txt","a")
+    output.close()
+    output = open("chain.txt","r+")
+    dict = dictfp.read()
+    dict = dict.split("\n")
+    dictfp.close()
+    del dictfp
+    next_word = "==end-Game=="
+    beforechain = output.read()
+    if(len(beforechain)==0):
+        next_word=getNewWord(output,dict)
+    else:
+        beforechain=beforechain.split("==end-Game==")
+        if(len(beforechain)>0):
+            beforechain=beforechain[-1]
+        else:
+            beforechain=beforechain[0]
+        beforechain=beforechain.split("\n")
+        #if(len(beforechain)>1 and len(beforechain[-1])==0):
+        #    del beforechain[-1]
+        tmplst = []
+        for v in beforechain:
+            if(len(v)>1):
+                tmplst.append(v)
+        del beforechain
+        beforechain=tmplst
+        if(len(beforechain)<1 ):
+            new_word= getNewWord(output,dict)
+            output.close()
+            return new_word
+        prefix=str((beforechain[-1])[-1])
+        if(prefix!="="):
+            available_wordlist=[]
+            for someword in dict:
+                if(len(someword)>1 and someword not in beforechain and someword[0]==prefix):
+                    available_wordlist.append(someword)
+            if(len(available_wordlist)>0):      
+                next_word=available_wordlist[random.randint(0,len(available_wordlist)-1)]
+                
+            #print("show data before append word: ",ne)
+            output.write(next_word+"\n")
+        else:
+            next_word = getNewWord(output,dict)
+    output.close()
+    del output
+    return next_word
+doWordChain()
