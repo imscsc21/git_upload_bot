485c10b04a136fc84dcefaf510b550614145f069
DoRan <imscsc21@naver.com>
2016-11-11 19:05:47 +0900

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,9 +2,10 @@
 cd $2
 git config --global user.name "hsh97Erica"
 git config --global user.email "imscs21@hanyang.ac.kr"
-time=$((60*60*6))
-for i in $(seq 1 3)
+time=$((60*60*6))#loop per 6 hours
+for i in $(seq 1 99999)
 do
+echo $i 번째 구동
 cd $1
 python3 wordchain.py
 cd $2
