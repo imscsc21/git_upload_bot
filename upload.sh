9e0850b05908a9d6e8cb34f1d41000158b02e225
DoRan <imscsc21@naver.com>
2016-11-11 10:05:29 +0900

    aa

--- upload.sh
+++ upload.sh
@@ -2,10 +2,10 @@
 cd $1
 python3 wordchain.py
 cd $2
-git config user.name "hsh97Erica"
-git config user.email "imscs21@hanyang.ac.kr"
+git config --global user.name "hsh97Erica"
+git config --global user.email "imscs21@hanyang.ac.kr"
 time=60
-((time*=60*24))
+$((time*=60*24))
 #for i in {1..10000}
 #do
 git init
