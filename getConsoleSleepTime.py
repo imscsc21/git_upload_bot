c6e585bb5ac15c1c117cc309b947b4ae96875b3e
DoRan <imscsc21@naver.com>
2016-11-12 22:19:20 +0900

    Add files via upload

--- getConsoleSleepTime.py
+++ getConsoleSleepTime.py
@@ -1,6 +1,11 @@
 import random
 unit=60*60 #hour
-tmp=random.randrange(0,10)
+tmp=random.randrange(0,8)
+rrange=60*30
+randomvar=random.randrange(-1*rrange,rrange)
 if(tmp>6):
     tmp=6
-print(unit*tmp)
+tmptime=unit*tmp
+if(tmptime+randomvar>=0):
+    tmptime+=randomvar
+print(tmptime)
